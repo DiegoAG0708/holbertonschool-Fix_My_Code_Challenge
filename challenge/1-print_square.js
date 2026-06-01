f6329a06fd364a3d89671edec0dc7f0d667d9eff
root <root@ip-10-42-214-8.ec2.internal>
2026-06-01 18:42:44 +0000

    Fix Print square: enforce base 10 parsing

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2], 10);  // force base 10
 
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {        // loop exactly 'size' times
-    console.log('#'.repeat(size));        // each line has 'size' hashes
+  for (let i = 0; i < size; i++) {
+    console.log('#'.repeat(size));
   }
 }
