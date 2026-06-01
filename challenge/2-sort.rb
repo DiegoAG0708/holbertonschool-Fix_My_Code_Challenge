d34a6672a49c7218b3b7d6e14e391e1386f10b59
root <root@ip-10-42-214-8.ec2.internal>
2026-06-01 18:51:34 +0000

    Fix Sort: ignore non-integer arguments before sorting

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
-# Sort arguments numerically
+# Sort arguments numerically, ignoring non-integers
 
-args = ARGV.map(&:to_i)   # convert all arguments to integers
+args = ARGV.select { |a| a.match?(/^[-]?\d+$/) }  # keep only valid integers
+args.map!(&:to_i)
 args.sort.each do |n|
   puts n
 end
