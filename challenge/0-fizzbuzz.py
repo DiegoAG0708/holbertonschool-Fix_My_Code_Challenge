0fdece051b0a3be40b75ca78f8b6efb8645f9aa4
root <root@ip-10-42-214-8.ec2.internal>
2026-06-01 18:34:01 +0000

    Fix FizzBuzz: check divisibility by 3 and 5 first

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -3,7 +3,7 @@ import sys
 
 def fizzbuzz(n):
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:   # check FizzBuzz first
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
